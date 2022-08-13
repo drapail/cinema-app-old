bfce7779f870f931cf30cbf6baef1628972b0b76
drapail <mv.dibrov@gmail.com>
2022-08-13 11:54:57 +0300

    stylecheck plugin added

---
+++ src/main/java/cinema/service/impl/OrderServiceImpl.java
@@ -0,0 +1,40 @@
+package cinema.service.impl;
+
+import cinema.dao.OrderDao;
+import cinema.lib.Inject;
+import cinema.lib.Service;
+import cinema.model.Order;
+import cinema.model.ShoppingCart;
+import cinema.model.Ticket;
+import cinema.model.User;
+import cinema.service.OrderService;
+import cinema.service.ShoppingCartService;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    @Inject
+    private OrderDao orderDao;
+    @Inject
+    private ShoppingCartService shoppingCartService;
+
+    @Override
+    public Order completeOrder(ShoppingCart shoppingCart) {
+        User user = shoppingCart.getUser();
+        List<Ticket> tickets = new ArrayList<Ticket>(shoppingCart.getTickets());
+        LocalDateTime now = LocalDateTime.now();
+        Order order = new Order();
+        order.setUser(user);
+        order.setTickets(tickets);
+        order.setOrderDate(now);
+        shoppingCartService.clearShoppingCart(shoppingCart);
+        return orderDao.add(order);
+    }
+
+    @Override
+    public List<Order> getOrdersHistory(User user) {
+        return orderDao.getAllByUser(user);
+    }
+}
